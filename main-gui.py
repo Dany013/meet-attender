8d70b50db46593cf62b44c23b264fb126939a612
Dany013 <80245493+Dany013@users.noreply.github.com>
2021-03-10 19:04:45 +0100

    Update main-gui.py

--- main-gui.py
+++ main-gui.py
@@ -1,5 +1,5 @@
 import os
-import threading
+#import threading
 import tkinter as tk
 
 os.chdir(os.getcwd())
@@ -34,26 +34,26 @@ def gdlnk():
     os.system("guide-links.txt")
 
 
-ch_time = tk.Button(self, text="CHANGE SCHEDULE ", command=exc).grid(
-    row=0, column=0, sticky="nsw"
+ch_time = tk.Button(self, text="CHANGE SCHEDULE", command=exc).grid(
+    row=0, column=0, sticky="nswe"
 )
-ch_link = tk.Button(self, text=" CHANGE LINKS  ", command=lnk).grid(
-    row=0, column=1, sticky="nsw"
+ch_link = tk.Button(self, text="CHANGE LINKS", command=lnk).grid(
+    row=0, column=1, sticky="nswe"
 )
-guid_time = tk.Button(self, text="    guide schedule      ", command=gdsch).grid(
-    row=1, column=0, sticky="nsw"
+guid_time = tk.Button(self, text="guide schedule", command=gdsch).grid(
+    row=1, column=0, sticky="nswe"
 )
-guid_link = tk.Button(self, text="     guide links      ", command=gdlnk).grid(
-    row=1, column=1, sticky="nsw"
+guid_link = tk.Button(self, text="guide links", command=gdlnk).grid(
+    row=1, column=1, sticky="nswe"
 )
 start = tk.Button(
     self,
-    text="             START             ",
+    text=" START",
     bg="green",
     activebackground="green",
     command=strt,
-).grid(row=2, column=0, sticky="nsw")
-bad_gui = tk.Label(self, text="really bad GUI :)").grid(row=2, column=1, sticky="nsw")
+).grid(row=2, column=0, sticky="nswe")
+bad_gui = tk.Label(self, text="really bad GUI :)").grid(row=2, column=1, sticky="nswe")
 
 if __name__ == "__main__":
-    self.mainl
+    self.mainloop()
