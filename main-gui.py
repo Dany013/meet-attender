5aef5e6266d24261d67a1d2d215e52dd1af5c5b1
DanyB0 <daniele.beltrami13@gmail.com>
2021-03-10 15:47:59 +0100

    Update main-gui.py

--- main-gui.py
+++ main-gui.py
@@ -9,6 +9,7 @@ self.title("meet")
 self.resizable(0, 0)
 self.grid()
 
+
 def stop():
     os.startfile("stop.bat")
 
@@ -33,12 +34,26 @@ def gdlnk():
     os.system("guide-links.txt")
 
 
-ch_time = tk.Button(self, text="CHANGE SCHEDULE ", command=exc).grid(row=0, column=0, sticky="nsw")
-ch_link = tk.Button(self, text=" CHANGE LINKS  ", command=lnk).grid(row=0, column=1, sticky="nsw")
-guid_time = tk.Button(self, text="    guide schedule      ", command=gdsch).grid(row=1, column=0, sticky="nsw")
-guid_link = tk.Button(self, text="     guide links      ", command=gdlnk).grid(row=1, column=1, sticky="nsw")
-start = tk.Button(self, text="             START             ", bg="green", activebackground="green", command=strt).grid(row=2, column=0, sticky="nsw")
+ch_time = tk.Button(self, text="CHANGE SCHEDULE ", command=exc).grid(
+    row=0, column=0, sticky="nsw"
+)
+ch_link = tk.Button(self, text=" CHANGE LINKS  ", command=lnk).grid(
+    row=0, column=1, sticky="nsw"
+)
+guid_time = tk.Button(self, text="    guide schedule      ", command=gdsch).grid(
+    row=1, column=0, sticky="nsw"
+)
+guid_link = tk.Button(self, text="     guide links      ", command=gdlnk).grid(
+    row=1, column=1, sticky="nsw"
+)
+start = tk.Button(
+    self,
+    text="             START             ",
+    bg="green",
+    activebackground="green",
+    command=strt,
+).grid(row=2, column=0, sticky="nsw")
 bad_gui = tk.Label(self, text="really bad GUI :)").grid(row=2, column=1, sticky="nsw")
 
 if __name__ == "__main__":
-    self.mainloop()
+    self.mainl
